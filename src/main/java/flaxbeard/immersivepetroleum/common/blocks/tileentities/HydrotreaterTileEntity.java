153b21293f112121f2370bf34843fb7440facd08
Aubri Seebauer <bananacreature@gmail.com>
2022-06-02 17:19:18 -0400

    Hydrotreater tick order change

--- src/main/java/flaxbeard/immersivepetroleum/common/blocks/tileentities/HydrotreaterTileEntity.java
+++ src/main/java/flaxbeard/immersivepetroleum/common/blocks/tileentities/HydrotreaterTileEntity.java
@@ -212,6 +212,7 @@ public class HydrotreaterTileEntity extends PoweredMultiblockTileEntity<Hydrotre
 	
 	@Override
 	public void tick(){
+		super.tick();
 		checkForNeedlessTicking();
 		
 		if(this.world.isRemote || isDummy() || isRSDisabled()){
@@ -252,7 +253,6 @@ public class HydrotreaterTileEntity extends PoweredMultiblockTileEntity<Hydrotre
 			update = true;
 		}
 		
-		super.tick();
 		
 		if(this.tanks[TANK_OUTPUT].getFluidAmount() > 0){
 			BlockPos outPos = getBlockPosForPos(Fluid_OUT).up();
